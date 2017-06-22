ec685665bcae0801047e735a1dde360a322ea53a
Nenonen Mikael <k90390@pitohui.org.aalto.fi>
2017-06-22 15:21:39 +0300

    Taulukkoharjoittelua coming to a close. Needs testing.

--- taulukkoharjoittelua/taulukkoharjoittelua.c
+++ taulukkoharjoittelua/taulukkoharjoittelua.c
@@ -20,26 +20,27 @@ int array_sum(int *array, int count) {
 
 int array_reader(int *vals, int n) {
     char line[120];
-	printf("Anna lukuja.\n");
-    int result = scanf("%s", &line);
-	printf(line);
-    if(result) {
-	} else {
+    printf("Anna lukuja.\n");
+	int i=0;
+    while(1) {
+	    int result = scanf("%d", &vals[i]);
+		++i;
+		if(result == 0) 
+			break;
 	}
-	printf(line);
-    //for (int i = 0; i < n; ++i) {
-        
-    //}
-    return 4;
+	return i;
 }
 
 int main(void) {
     int vals[] = {33, 35, 36};
-	vals[0] = array_reader(vals, 3);
-	char my_sign = (char)array_sum(vals, 3);
-	printf("heis %c %d \n", my_sign, my_sign);
+	int vals2[20] = {};
+	int arvoja = array_reader(vals2, 20);
+	//char my_sign = (char)array_sum(vals, 3);
+	//printf("heis %c %d \n", my_sign, my_sign);
 	
 	int valarray[] = { 10, 100, 1000 };
 	int ret = array_sum(valarray, 3);
-	printf("Sum %d", ret);
+	printf("Sum %d\n", ret);
+	printf("Array_reader says %d\n", arvoja);
+	printf("%d %d %d %d\n", vals2[0], vals2[1], vals2[2], vals2[3]);
 }
