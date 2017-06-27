b580ed35f8f0bf9e0e6ea2da5ba5b8e12c2e2979
Mikael Nenonen <mikael.nenonen@iki.fi>
2017-06-27 22:11:06 +0300

    The words now appear where they need to, but there are extra spaces.

--- korsoraattori/korsoraattori.c
+++ korsoraattori/korsoraattori.c
@@ -24,6 +24,31 @@ int check_for_ts(char *dest, const char *src) {
 	return 0;
 }
 
+int check_if_word_needed(char *dest, const char *src) {
+	static int spaces_in_loop = 0;
+	if (*src == 0) {			// My magic to reset the static counter
+		printf("spaces: %d", spaces_in_loop);
+		spaces_in_loop = 0;
+		printf("spaces: %d", spaces_in_loop);
+		return 0;
+	}
+	int added = 0;
+	if (*src == ' ') {
+		spaces_in_loop++;
+		if (spaces_in_loop%3 == 0 && spaces_in_loop > 0) { // niinku coming
+			printf("c() Dest nyt: %s\n", dest);
+			strcpy(dest, " niinku ");
+			printf("c() Dest nyt: %s\n", dest);
+			added += 8;
+		}
+		if (spaces_in_loop%4 == 0 && spaces_in_loop > 0) { // totanoin coming
+			strcpy(dest, " totanoin ");
+			added += 10;
+		}		
+	}
+	return added;
+}
+
 void korsoraattori(char *dest, const char *src) {
 	for(int i=0; *(dest+i) && *src; i++) {
 		if (check_for_ks((dest+i), src)) {
@@ -34,18 +59,31 @@ void korsoraattori(char *dest, const char *src) {
 			src += 2;
 			continue;
 		}
+		printf("k() Dest nyt: %s\n", dest);
+		int added = check_if_word_needed(dest+i, src);
+		i += added;
+		printf("k() Dest nyt: %s\n", dest);
 		*(dest+i) = *src;
 		src++;
 	}
 	printf(dest);
 }
 
-int main() {
-	char dest[180];
-	for (int i=0; i<180; i++) {
+void init_array(char *dest, int size) {
+	for (int i=0; i<size; i++) {
 		dest[i] = 1;
 	}
-	char src[] = "Nuorna vitsa väännettävä, ja mustan kissan paksut posket.";
+}
+
+int main() {
+	char dest[180];
+	init_array(dest, 180);
+	char src[] = "Nuorna vitsa väännettävä, ja";
 	korsoraattori(dest, src);
+	printf("\n");
+	init_array(dest, 180);
+	check_if_word_needed((char*)"", (char*)"");
+	char src2[] = "Nuorna vitsa väännettävä, ja mustan kissan paksut posket.";
+	korsoraattori(dest, src2);
 	return 0;
 }
