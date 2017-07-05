df33ecc1f08fafe35247cc7c04463110cbe1af51
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-05 21:46:45 +0300

    Passing a pointer to the beginning of allocated memory. The first function is rated as working.

--- dyn_array/dyn_array.c
+++ dyn_array/dyn_array.c
@@ -6,13 +6,14 @@
 
 int *dynamic_reader(unsigned int n) {
   int *p = (int *) malloc(n*sizeof(int));
+  int *copy = p;
   if(p) { // allocation worked
     int i;
     for(i=0; i<n; i++) {
       scanf("%d", p);
       p++;
     }
-    return p;
+    return copy;
   }
   return 0;
 }
@@ -24,6 +25,8 @@ int *insert_into_array(int *arr, unsigned int num, int newval) {
 int main(void) {
   int *p = dynamic_reader(3);
   if(!p)
+    free(p);
     return 1;
+  free(p);
   return 0;
 }
