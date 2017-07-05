ad56965da91653bb0a19f1c4f226b8136a2a0ac8
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-05 21:17:29 +0300

    Empty methods.

---
+++ dyn_array/dyn_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Mikael Nenonen k90390
+// 2017-07-05
+
+int *dynamic_reader(unsigned int n) {
+  return 0;
+}
+
+int *insert_into_array(int *arr, unsigned int num, int newval) {
+  return 0;
+}
+
+int main(void) {
+  return 0;
+}
