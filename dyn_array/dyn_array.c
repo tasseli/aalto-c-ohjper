bb62072c8845a1b2b24f843c1eeaa323db0456f6
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-05 21:40:04 +0300

    Raw first version for dynamic_reader

--- dyn_array/dyn_array.c
+++ dyn_array/dyn_array.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Mikael Nenonen k90390
 // 2017-07-05
 
 int *dynamic_reader(unsigned int n) {
+  int *p = (int *) malloc(n*sizeof(int));
+  if(p) { // allocation worked
+    int i;
+    for(i=0; i<n; i++) {
+      scanf("%d", p);
+      p++;
+    }
+    return p;
+  }
   return 0;
 }
 
@@ -12,5 +22,8 @@ int *insert_into_array(int *arr, unsigned int num, int newval) {
 }
 
 int main(void) {
+  int *p = dynamic_reader(3);
+  if(!p)
+    return 1;
   return 0;
 }
