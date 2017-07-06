8f39fc5ae5830bbe343a6bd4673043be3bcbdaed
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-06 22:19:57 +0300

    Compiles but doesn't operate. Finish up tomorrow, debugging loops and accurate comment sizes.

--- delete_comments/delete_comments.c
+++ delete_comments/delete_comments.c
@@ -6,12 +6,6 @@
 // 2017-07-06
 
 char *delete_comments(char *input) {
-    enum state {
-        NOT_IN_COMMENT,
-        IN_LINE_COMMENT,
-        IN_COMMENT_BLOCK
-    } mystate;
-    mystate = NOT_IN_COMMENT;
     char *len_counter = input;
     int input_length = 0;
     while(*len_counter) {
@@ -20,32 +14,75 @@ char *delete_comments(char *input) {
     char *input_copy = input;
     while(*input_copy) {
         if (*input == '/') {
-            if(mystate == NOT_IN_COMMENT) {
-                if (*(input+1) == '/') {
-                    // line comment starts
-                    input += 2;
-                    char *copy = input;
-                    int comment_length = 0;
-                    while(*copy != '\n') {
-                        copy++, comment_length++;
-                    }
-                    
+            if (*(input+1) == '/') {
+                // line comment starts
+                char *copy = input;
+                int comment_length = 0;
+                while(*copy != '\n') {
+                    copy++, comment_length++;
                 }
-                if (*(input+1) == '*') {
-                    // block comment starts
+                char *rest = copy;
+                int rest_length = 0;
+                while(*copy) {
+                    rest_length++, rest++;
                 }
-            } else if(mystate == IN_LINE_COMMENT) {
-                
-            } else if(mystate == IN_COMMENT_BLOCK) {
-                
+                // move the rest of the input to replace the comment. Memmove?
+                input = (char *) memmove(input, copy, rest_length);
+            }
+            if (*(input+1) == '*') {
+                // block comment starts
+                char *copy = input;
+                int comment_length = 0;
+                while(*copy != '*' && *(copy+1) != '/') {
+                    copy++, comment_length++;
+                }
+                // add one or two to compensate for the longer comment, and move as before.
             }
         }
         input++;
     }
-    return 0;
+    return input;
 }
 
-int main(void) {
-    printf("Hello world.\n");
-    return 0;
+/* Read given file <filename> to dynamically allocated memory.
+ * Return pointer to the allocated memory with file content, or
+ * NULL on errors.
+ */
+char *read_file(const char *filename)
+{
+    FILE *f = fopen(filename, "r");
+    if (!f)
+        return NULL;
+    
+    char *buf = NULL;
+    unsigned int count = 0;
+    const unsigned int ReadBlock = 100;
+    unsigned int n;
+    do {
+        buf = realloc(buf, count + ReadBlock + 1);
+        n = fread(buf + count, 1, ReadBlock, f);
+        count += n;
+    } while (n == ReadBlock);
+    
+    buf[count] = 0;
+    
+    return buf;
+}
+
+
+int main(void)
+{
+    char *code = read_file("testifile.c");
+    if (!code) {
+        printf("No code read");
+        return -1;
+    }
+    printf("-- Original:\n");
+    fputs(code, stdout);
+    
+    code = delete_comments(code);
+    printf("-- Comments removed:\n");
+    fputs(code, stdout);
+    
+    free(code);
 }
