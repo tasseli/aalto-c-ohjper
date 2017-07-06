d8c2e667d8137de830ea513d25e60c13586223c6
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-06 16:39:23 +0300

    Putting the program skeleton together.

--- join_arrays/join-arrays.c
+++ join_arrays/join-arrays.c
@@ -5,7 +5,23 @@
 // Mikael Nenonen k90390
 // 2017-07-06
 
-int main(void) {
-  printf("Hello world.\n");
-  return 0;
+int *join_arrays(unsigned int s1, int *a1, unsigned int s2, int *a2, unsigned int s3, int *a3) {
+	return 0;
+}
+
+int main(void)
+{
+    /* testing exercise. Feel free to modify */
+    int a1[] = { 1, 2, 3, 4, 5 };
+    int a2[] = { 10, 11, 12, 13, 14, 15, 16, 17 };
+    int a3[] = { 20, 21, 22 };
+
+    int *joined = join_arrays(5, a1, 8, a2, 3, a3);
+
+    for (int i = 0; i < 5 + 8 + 3; i++) {
+        printf("%d  ", joined[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
