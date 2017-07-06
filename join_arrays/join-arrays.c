b6eb6e8f630efbafc5730ef649c148957dcba905
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-06 18:03:07 +0300

    join_arrays got full points on TIM.

--- join_arrays/join-arrays.c
+++ join_arrays/join-arrays.c
@@ -6,7 +6,13 @@
 // 2017-07-06
 
 int *join_arrays(unsigned int s1, int *a1, unsigned int s2, int *a2, unsigned int s3, int *a3) {
-	return 0;
+    int *result = (int *) malloc((s1+s2+s3)*sizeof(int));
+    memcpy(result, (void *) a1, s1*sizeof(int));
+    int *beginning_of_2 = &result[s1];
+    memcpy(beginning_of_2, a2, s2*sizeof(int));
+    int *beginning_of_3 = &result[s1+s2];
+    memcpy(beginning_of_3, a3, s3*sizeof(int));
+    return result;
 }
 
 int main(void)
