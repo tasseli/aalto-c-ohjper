aeee8f3be2e7bbbb72c56dd2aa16778fc5d8dd51
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-07-06 19:27:07 +0300

    delete_comments - line comment remover coming up.

--- delete_comments/delete_comments.c
+++ delete_comments/delete_comments.c
@@ -12,10 +12,27 @@ char *delete_comments(char *input) {
         IN_COMMENT_BLOCK
     } mystate;
     mystate = NOT_IN_COMMENT;
-    
+    char *beginning = input;
     while(*input) {
         if (*input == '/') {
-            
+            if(mystate == NOT_IN_COMMENT) {
+                if (*(input+1) == '/') {
+                    // line comment starts
+                    input += 2;
+                    char *copy = input;
+                    int counter = 0;
+                    while(*copy != '\n') {
+                        copy++, counter++;
+                    }
+                }
+                if (*(input+1) == '*') {
+                    // block comment starts
+                }
+            } else if(mystate == IN_LINE_COMMENT) {
+                
+            } else if(mystate == IN_COMMENT_BLOCK) {
+                
+            }
         }
         input++;
     }
