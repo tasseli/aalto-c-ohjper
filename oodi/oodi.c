e2f1c9ad469fec201d36de955cf13ac4f9e6bde6
Mikael Nenonen <mikael.nenonen@iki.fi>
2017-07-10 22:27:09 +0300

    One third of Oodi points. Do two more functions.

--- oodi/oodi.c
+++ oodi/oodi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "oodi.h"
 
 // Mikael Nenonen k90390
 // 2017-07-10
@@ -19,8 +20,30 @@ int init_record(struct oodi *or, const char *p_student, const char *p_course,
     return 0;
   
   strcpy(or->student, p_student);
-  or->course = p_course;
+  int course_array_length = 1;  // There's the end sign
+  const char* p_course_copy = p_course;
+  while(*p_course_copy) {
+    course_array_length++;
+    p_course_copy++;
+  }
+  or->course = (char *) malloc(course_array_length);
+  if(!or->course)
+    return 0;
+  strcpy(or->course, p_course);
   or->grade = p_grade;
   or->compdate = p_compdate;
   return 1;
+}
+
+struct oodi *add_record(struct oodi *array, unsigned int size, struct oodi newrec) {
+  return 0;
+}
+
+int change_grade(struct oodi *array, unsigned int size, const char *p_student,
+        const char *p_course, unsigned char newgrade, struct date newdate) {
+  return 0;
+}
+
+int main(void) {
+  return 0;
 }
