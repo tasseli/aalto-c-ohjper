f9d1e3f94f22cf5d47a281e4bd6055a16bf7b1e6
Mikael Nenonen <mikael.nenonen@iki.fi>
2017-07-10 14:56:29 +0300

    Downloaded and modified source.h

---
+++ truck/source.h
@@ -0,0 +1,21 @@
+
+// Cargo: what the truck is carrying
+struct cargo {
+    const char *title;
+    int quantity;
+    double weight;
+};
+
+// Define the truck structure here
+struct truck{
+  // Have truck data
+  char name[31];
+  double length;
+  double weight;
+  struct cargo crg;
+}
+
+struct truck create_truck(const char *name, double length, double weight,
+			    struct cargo crg);
+
+void print_truck(const struct truck *car);
