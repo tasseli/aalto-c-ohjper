54963ff09b486fe11426e9c31fdc23f80442746a
Mikael Nenonen <mikael.nenonen@iki.fi>
2017-07-10 15:03:20 +0300

    Wrote create_truck() first version.

--- truck/truck.c
+++ truck/truck.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "source.h"
 
 // Mikael Nenonen k90390
 // 2017-07-10
 
-struct truck{
-  // Have truck data
-}
 typedef struct truck TR;
 
-TR create_truck() {
-  TR* trp = (TR) malloc(sizeof(TR));
+TR create_truck(const char *name, double length, double weight, struct cargo crg) {
   // Create a new truck in &trp
-  return trp;
+  TR creation;
+  creation.length = length;
+  creation.weight = weight;
+  creation.crg = crg;
+  int i;
+  for(i=0; i<31; i++) {
+    creation.name[i] = name[i];
+  }
+  return creation;
 }
 
-void print_truck(TR) {
+void print_truck(const TR *car) {
   // Print truck data
 }
 
