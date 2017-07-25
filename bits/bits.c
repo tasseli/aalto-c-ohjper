909c663eec594c122e0d5e9da0521693230d5aa7
Mikael Nenonen <test@mailinator.com>
2017-07-25 04:10:34 +0300

    Added empty function definitions and a bare-bones try for bit_set().

--- bits/bits.c
+++ bits/bits.c
@@ -5,5 +5,35 @@
 #include "bits.h"
 
 void bit_set(unsigned char* data, int idx) {
-  
+  int charpos = idx/8;
+  int withincharpos = idx%8;
+  data += charpos;
+  if(withincharpos == 0)
+    *data = *data | 0x80;
+  if(withincharpos == 1)
+    *data = *data | 0x40;
+  if(withincharpos == 2)
+    *data = *data | 0x20;
+  if(withincharpos == 3)
+    *data = *data | 0x10;
+  if(withincharpos == 4)
+    *data = *data | 0x8;
+  if(withincharpos == 5)
+    *data = *data | 0x4;
+  if(withincharpos == 6)
+    *data = *data | 0x2;
+  if(withincharpos == 7)
+    *data = *data | 0x1;
+}
+
+int bit_get(const unsigned char* data, int idx) {
+}
+
+void bit_unset(unsigned char* data, int idx) {
+}
+
+unsigned char bit_get_sequence(const unsigned char* data, int idx, int how_many) {
+}
+
+void print_byte(unsigned char b) {
 }
