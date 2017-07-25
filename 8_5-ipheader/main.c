a1def5ff73b7d3b018a974984a016398fe691ea6
Mikael Nenonen <test@mailinator.com>
2017-07-25 17:25:36 +0300

    Added the supplied files.

---
+++ 8_5-ipheader/main.c
@@ -0,0 +1,20 @@
+#include <arpa/inet.h>
+#include "ipheader.h"
+
+int main()
+{
+    /* Feel free to modify this function to test different things */
+    
+    unsigned char bytes[] = {
+        0x45, 0x00, 0x01, 0xd3, 0xda, 0x8d, 0x40, 0x00,
+        0x40, 0x06, 0x8c, 0xd5, 0xc0, 0xa8, 0x01, 0x46,
+        0x6c, 0xa0, 0xa3, 0x33 };
+    
+    struct ipHeader ip;
+    
+    parseIp(&ip, bytes);
+    printIp(&ip);
+    hexdump(bytes, sizeof(bytes));
+
+    return 0;
+}
