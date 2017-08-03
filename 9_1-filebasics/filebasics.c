690880acc9a4c08c70f33e4d726501e11ebc89cc
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-08-03 19:30:10 +0300

    First function in 9_1 seems to work.

--- 9_1-filebasics/filebasics.c
+++ 9_1-filebasics/filebasics.c
@@ -6,6 +6,23 @@
 // 2017-08-03
 
 int print_file(const char *filename) {
-  
+  FILE *f = fopen(filename, "r");
+  if (!f) {
+    fprintf(stderr, "Opening file failed\n");
+    exit(EXIT_FAILURE);  // ends program immediately (in stdlib)
+  }
+  int length = 0;
+  while(1) {
+    int got = fgetc(f);
+    if(got == EOF)
+      break;
+    length++;
+    printf("%c", (char)got);
+  }
+  fclose(f);
+  return length;
+}
+
+char *difference(const char* file1, const char* file2) {
   return 0;
 }
