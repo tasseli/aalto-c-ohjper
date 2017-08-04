0d5bc2dad57c6b8138f0f7be8217c6a1f7531855
Mikael Nenonen <mikael.nenonen@iki.fi>
2017-08-04 23:01:28 +0300

    With a test file of my own, my code seems to work as I intended. But there's something seriously fishy to my eye in the test file provided.

--- 9_3-filestats/filestats.c
+++ 9_3-filestats/filestats.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 // Mikael Nenonen k90390
 // 207-08-04
@@ -23,6 +24,33 @@ int line_count(const char *filename) {
 }
 
 int word_count(const char *filename) {
-  
-  return 0;
+  FILE* f = fopen(filename, "r");
+  if(!f)
+    return -1;
+  int words = 0;
+  char line[1000] = "";
+  while(fgets(line, 1000, f) != NULL) {
+    int i;
+    int prev_was_space = 1;
+    int prev_was_alpha = 0;
+    for(i=0; i<1000 && line[i] != 0; i++) {
+      // handle lines beginning with whitespace, and space sequences.
+      if(prev_was_space == 1) {
+        if(isspace((int)line[i]))
+          continue;
+      }
+      // count a word as one only
+      if(isalpha((int)line[i])) {
+        if(prev_was_alpha == 0)
+          words++;
+        prev_was_alpha = 1;
+        prev_was_space = 0;
+      }
+      if(isspace((int)line[i])) {
+        prev_was_space = 1;
+        prev_was_alpha = 0;
+      }
+    }
+  }
+  return words;
 }
