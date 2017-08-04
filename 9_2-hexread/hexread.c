b71538d0da608d767aadc9156d214700af91a6e7
Mikael Nenonen <mikael.nenonen@iki.fi>
2017-08-04 22:00:03 +0300

    Built the framework for 9_2, next up printing.

--- 9_2-hexread/hexread.c
+++ 9_2-hexread/hexread.c
@@ -5,6 +5,19 @@
 // 2017-08-04
 
 int dump_hex(const char *filename) {
-  
-  return 0;
+  FILE* f;
+  f = fopen(filename, "r");
+  if(!f)
+    return -1;
+  int got, read=0;
+  while(!feof(f)) {
+    got = fgetc(f);
+    if(got == EOF)
+      break;
+    read++;
+    // print hex and a space
+    // every 16th mark, add \n
+  }
+  fclose(f);
+  return read;
 }
