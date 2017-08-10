ba47dfcbc1f06fac0ce3b3ff119f52c9d717f8e4
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-08-10 23:01:50 +0300

    First idea of findProduct written in 10_3

--- 10_3-shopfunctions/shopfunctions.c
+++ 10_3-shopfunctions/shopfunctions.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include "shopfunctions.h"
 
 // Mikael Nenonen k90390
 // 2017-08-10
@@ -15,6 +17,5 @@ int compareNum(const void* a, const void* b) {
 }
 
 Product* findProduct(Product* p_array, const char* searchkey, int (*cmp)(const void*, const void*)) {
-  
-  return NULL;
+  return (Product*)bsearch(searchkey, p_array, sizeof(p_array)/sizeof(Product), sizeof(Product), cmp);
 }
