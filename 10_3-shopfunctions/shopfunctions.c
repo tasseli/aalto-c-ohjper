4cca9a01822df2b5fce912a285d0451cb62205b3
Mikael Nenonen <mikael.nenonen@mailinator.com>
2017-08-10 22:35:02 +0300

    Begun on 10_3.

---
+++ 10_3-shopfunctions/shopfunctions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// Mikael Nenonen k90390
+// 2017-08-10
+
+int compareAlpha(const void* a, const void* b) {
+  
+  return 0;
+}
+
+int compareNum(const void* a, const void* b) {
+  
+  return 0;
+}
+
+Product* findProduct(Product* p_array, const char* searchkey, int (*cmp)(const void*, const void*)) {
+  
+  return NULL;
+}
